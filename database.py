b26cd6aeecf6d8fe10231cd72b1fefdcfb957232
narasimmankalyan <158766757+narasimmankalyan@users.noreply.github.com>
2026-04-17 14:24:57 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,14 @@
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
+
+MONGO_HOST = os.getenv("MONGODB_HOST")
+MONGO_PORT = os.getenv("MONGODB_PORT")
+MONGO_DB = os.getenv("MONGODB_DBNAME")
+MONGO_USER = os.getenv("MONGODB_USERNAME")
+MONGO_PASS = os.getenv("MONGODB_PASSWORD")
+
+MONGO_URL = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}"
+
+client = AsyncIOMotorClient(MONGO_URL)
+db = client[MONGO_DB]
+collection = db["students"]
