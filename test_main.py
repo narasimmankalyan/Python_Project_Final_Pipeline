400d006c8aad8b9b32a4dedb398a3a3f72495233
narasimmankalyan <158766757+narasimmankalyan@users.noreply.github.com>
2026-04-20 09:57:29 +0530

    Add files via upload

---
+++ test_main.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_create_student():
+    data = {
+        "name": "John",
+        "age": 20,
+        "course": "CS"
+    }
+    response = client.post("/students", json=data)
+    assert response.status_code == 200
+    assert response.json()["message"] == "Student stored successfully"
+
+
+def test_get_students():
+    response = client.get("/students")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
