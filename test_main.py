e59edbaeb8aa5f43a55907b7be3415a54a3ef0c6
narasimmankalyan <158766757+narasimmankalyan@users.noreply.github.com>
2026-04-20 10:55:08 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,9 +1,14 @@
+import pytest
 from fastapi.testclient import TestClient
-from main import app
+from app.main import app
 
-client = TestClient(app)
+@pytest.fixture(scope="module")
+def client():
+    with TestClient(app) as c:
+        yield c
 
-def test_create_student():
+
+def test_create_student(client):
     data = {
         "name": "John",
         "age": 20,
@@ -14,7 +19,7 @@ def test_create_student():
     assert response.json()["message"] == "Student stored successfully"
 
 
-def test_get_students():
+def test_get_students(client):
     response = client.get("/students")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
