880d65d763443db29fe7438aa4619d67ec0012e2
narasimmankalyan <158766757+narasimmankalyan@users.noreply.github.com>
2026-04-21 14:58:58 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,19 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_HOST = os.getenv("MONGODB_HOST")
-MONGO_PORT = os.getenv("MONGODB_PORT")
-MONGO_DB = os.getenv("MONGODB_DBNAME")
+MONGO_HOST = os.getenv("MONGODB_HOST", "localhost")
+MONGO_PORT = os.getenv("MONGODB_PORT", "27017")
+MONGO_DB = os.getenv("MONGODB_DBNAME", "studentdb")
 MONGO_USER = os.getenv("MONGODB_USERNAME")
 MONGO_PASS = os.getenv("MONGODB_PASSWORD")
 
-# MONGO_URL = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}"
+if MONGO_USER and MONGO_PASS:
+    MONGO_URL = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}"
+else:
+    MONGO_URL = f"mongodb://{MONGO_HOST}:{MONGO_PORT}"
 
-MONGO_URL = "mongodb://admin:testtesttest@localhost:27018"
+print("Connecting to Mongo:", MONGO_URL)  # 🔥 debug line
 
 client = AsyncIOMotorClient(MONGO_URL)
-db = client["studentdb"]
+db = client[MONGO_DB]
 collection = db["students"]
